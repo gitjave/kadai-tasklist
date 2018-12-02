2c3382dff66472269cc4ccc99b69c46cb3829786
git.jave <gitjave@gmail.com>
2018-12-02 12:31:17 +0000

    rooting revision

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  #get 'sessions/new'
 
-  get 'sessions/create'
+  #get 'sessions/create'
 
-  get 'sessions/destroy'
+  #get 'sessions/destroy'
 
-  get 'users/show'
+  #get 'users/show'
 
-  get 'users/new'
+  #get 'users/new'
 
-  get 'users/create'
+  #get 'users/create'
 
   root to: "tasks#index"
   
-  resources :tasks
+  resources :tasks, only:[:show, :new, :create, :edit, :update, :destroy]
   
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   
   get "signup", to: "users#new"
-  resources :users, only: [:show, :new, :create]
+  resources :users, only: [:show, :create]
   
 end
